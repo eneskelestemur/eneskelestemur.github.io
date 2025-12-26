f13a275fefad510606e793213630dee904943734
Enes Kelestemur <enesk@unc.edu>
2025-12-26 03:11:10 -0500

    Initial commit: clean slate

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Work in Progress</h1>
+      <p>
+        My name is Enes and I am building this site in React.
+      </p>
+    </div>
+  )
+}
+
+export default App
